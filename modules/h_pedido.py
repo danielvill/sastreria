334c8d9a4f89c4536a2f29a6d7b1b8da779575ed
Daniel Villacres <93736800+danielvill@users.noreply.github.com>
2025-01-31 18:29:01 -0500

    Cambio en el lado visual y necesito cambiar una tabla de h_pedido

--- modules/h_pedido.py
+++ modules/h_pedido.py
@@ -1,22 +1,29 @@
 class H_pedido:
-    def __init__(self,id_pedido,id_cliente,fecha_pedido,fecha_entrega,estado_pedido,productos,precio  ):
+    def __init__(self,id_pedido,id_cliente,id_producto,producto,cantidad,precio ,resultado,fecha_entrega,estado  ):
         
         self.id_pedido=id_pedido
         self.id_cliente=id_cliente
-        self.fecha_pedido=fecha_pedido
-        self.fecha_entrega=fecha_entrega
-        self.estado_pedido=estado_pedido
-        self.productos=productos
+        self.id_producto=id_producto
+        self.producto=producto
+        self.cantidad=cantidad
         self.precio=precio
+        self.resultado=resultado
+        self.fecha_entrega=fecha_entrega
+        self.estado=estado
         
+        
+
     def HpedidoDBCollection(self):
         return{
             "id_pedido": self.id_pedido,
             "id_cliente": self.id_cliente,
-            "fecha_pedido": self.fecha_pedido,
-            "estado_pedido": self.estado_pedido,
+            "id_producto": self.id_producto,
+            "producto": self.producto,
+            "cantidad": self.cantidad,
+            "precio": self.precio,
+            "resultado": self.resultado,
             "fecha_entrega": self.fecha_entrega,
-            "productos": self.productos,
-            "precio": self.precio
+            "estado": self.estado,
+            
         }
     
--- routes/h_pedido.py
+++ routes/h_pedido.py
@@ -4,3 +4,32 @@ from modules.h_pedido import H_pedido
 from pymongo import MongoClient
 db = dbase()
 h_pedido = Blueprint('h_pedido', __name__)
+
+
+@h_pedido.route("/admin/pedido", methods=['GET', 'POST'])
+def h_pedi():
+    
+    if request.method == 'POST':
+        h_pedido = db["h_pedido"]
+        pedido = db["pedido"]
+        id_pedido = request.form["id_pedido"]
+        id_cliente = request.form['id_cliente']
+        id_producto_list = request.form.getlist('id_producto')
+        producto_list = request.form.getlist('producto')
+        cantidad_list = request.form.getlist('cantidad')
+        precio_list = request.form.getlist('precio')
+        resultado_list = request.form.getlist('resultado')
+        fecha_entrega = request.form["fecha_entrega"]
+        estado =  request.form["estado"]
+        # Suponiendo que todos los lists son del mismo tamaño
+        for id_pedido, id_cliente, id_producto,producto, cantidad, precio, resultado , fecha_entrega,estado in zip(id_producto_list, producto_list, cantidad_list, precio_list, resultado_list):
+            h_ped = H_pedido(id_pedido, id_cliente, id_producto, producto, cantidad, precio, resultado,fecha_entrega,estado)
+            h_pedido.insert_one(h_ped.HpedidoDBCollection())
+        
+        # Eliminar todos los documentos en la colección carrito
+        
+        return redirect(url_for('h_pedido.gracias'))
+    else:
+        return render_template('admin/pedido.html')
+
+
